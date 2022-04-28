8f8ad1a5ae4c6876859269541d4125fda665790d
AlenkaNahorna <agyloma@gmail.com>
2022-04-28 21:42:55 +0200

    finished task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onGalleryClick);
+
+let instance = null;
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  instance = basicLightbox.create(`<img
+      class="gallery__image"
+      src = "${evt.target.dataset.source}"
+    />`);
+
+  instance.show();
+}
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && instance) {
+    instance.close();
+  }
+});
